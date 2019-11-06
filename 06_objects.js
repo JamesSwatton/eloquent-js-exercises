946167eb82ead328fb27e20528fdfeb77961db3f
JamesSwatton <jamessswatton@gmail.com>
2019-11-06 13:49:56 +0000

    add Anonther Cell

--- 06_objects.js
+++ 06_objects.js
@@ -1,4 +1,5 @@
 // A Vector Type
+// -------------
 // Write a constructor 'Vector' that represents a vector in two-dimensional space.
 // It takes 'x' and 'y' parameters (numbers), which it should save to properties of the same name.
 // Give the 'Vector' prototype two methods, 'plus' and 'minus', that take another vector as
@@ -22,3 +23,27 @@ Object.defineProperty(Vector.prototype, "length", {
 });
 
 console.log(new Vector(2, 2).length);
+
+
+// Another Cell
+// ------------
+// Implement a cell type named 'StretchCell(inner, width, height)' that conforms to the table cell
+// interface described earlier in the chapter. it should wrap another cell (like 'UnderlinedCell' does)
+// and ensure that the resulting cell has at least the given 'width' and 'height', even if the inner
+// cell would naturally be smaller.
+
+function StretchCell(inner, width, height) {
+    this.inner = inner;
+    this.width = width;
+    this.height = height;
+}
+StretchCell.prototype.minHeight = function() {
+    return Math.max(this.height, this.inner.minHeight());
+};
+StretchCell.prototype.minWidth = function() {
+    return Math.max(this.width, this.inner.minWidth());
+};
+StretchCell.prototype.draw = function(width, height) {
+    return this.inner.draw(width, height);
+}
+
